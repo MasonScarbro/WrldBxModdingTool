92b73417c64faab3dbb83d068a944116d4653835
MasonScarbro <masonscarbropf@gmail.com>
2023-10-03 00:04:12 -0400

    Almost done with efffects, cleaned things up | WIP

--- Main.py
+++ Main.py
@@ -5,40 +5,14 @@ import os
 import numpy as np
 import array as arr
 import string
-"""
-NOTES:
-     if os.path.exists('NewTraits.cs'):
-        #additional lines:
-        with open('NewTraits.cs', 'a') as f:
-                
-    else:
-        #starting code logic
-        with open('NewTraits.cs', 'a') as f:
-            f.write("using System; \n" 
-                + "using System.Threading; \n" 
-                + "using NCMS; \n" 
-                + "using UnityEngine; \n"
-                + "using ReflectionUtility; \n"
-                + "using System.Text; \n"
-                + "using System.Collections.Generic; \n"
-                + "using System.Linq; \n"
-                + "using System.Text; \n"
-                + "using ai; \n")
 
-I will probably have to add the initial and new to a string
-      that gets written to a file at teh end of create a mod that 
-      way we can make the file names based on the modname at the end
-      or we could do it this way and make a counter fo each time they 
-      click finish in order to make the file names the name concatenated with the 
-      counter but the first idea is better
-"""
-
+# ROOT DECLARATION #
 root = ctk.CTk(fg_color="#101519")
 root.geometry("1080x720")
-#frames add frames to have more things
 
 
-#CONSTANTS:
+
+# ---------------------- CONSTANTS ---------------------- #
 
 TRAIT_CODE_BEGINNING = ("using System; \n" 
                         "using System.Threading; \n"
@@ -57,7 +31,32 @@ TRAIT_CODE_BEGINNING = ("using System; \n"
                         "\n"
                         "\t\t public static void init() \n\t\t{")
 
-TRAIT_CODE_ENDING = "\n\n" + "\t\t}" + "\n" +"\n \t\t//OTHER FUNCTIONS GO HERE i.e custom death effects etc. \n" + "\n" + "\t}" + "\n" + "}" 
+EFFECTS_CODE_BEGINNING = TRAIT_CODE_BEGINNING.replace("Traits", "Effects")
+
+TRAIT_CODE_ENDING = ("\n\n"
+                     "\t\t}"
+                     "\n"
+                     "\n \t\t//OTHER FUNCTIONS GO HERE i.e custom death effects etc. \n"
+                     "\n"
+                     "\t\tpublic static void addTraitToLocalizedLibrary(string id, string description)"
+                     "\n\t\t{"
+                     "\n"
+                     '\n\t\t\tstring language = Reflection.GetField(LocalizedTextManager.instance.GetType(), LocalizedTextManager.instance, "language") as string;'
+                     '\n\t\t\tDictionary<string, string> localizedText = Reflection.GetField(LocalizedTextManager.instance.GetType(), LocalizedTextManager.instance, "localizedText") as Dictionary<string, string>;'
+                     '\n\t\t\tlocalizedText.Add("trait_" + id, id);'
+                     '\n\t\t\tlocalizedText.Add("trait_" + id + "_info", description);'
+                     '\n\t\t}'
+                     "\n"
+                     "\t}"
+                     "\n"
+                     "}") 
+
+
+
+EFFECTS_CODE_ENDING = (TRAIT_CODE_ENDING
+                       .replace('localizedText.Add("trait_" + id, id);', 'localizedText.Add(name, id);' )
+                       .replace('localizedText.Add("trait_" + id + "_info", description);', 'localizedText.Add(description, description);')
+                       .replace('addTraitToLocalizedLibrary(string id, string description)', 'localizeStatus(string id, string name, string description)')) 
 
 MAIN_CODE = ("using System; \n"
              "using NCMS; \n"
@@ -77,7 +76,12 @@ MAIN_CODE = ("using System; \n"
              "\t}\n"
              "}"
              )
-#BUTTON LOGIC FUNCTIONS:
+
+# ---------------------------------------------------------- #
+
+
+
+# ---------------------- BUTTON COMMANDS ---------------------- #
 string = ''
 traitsArr = []
 def traitCreate():
@@ -115,50 +119,108 @@ def traitCreate():
     
 def write():
     with open('NewTraits.cs', 'a') as f:
-            f.write(string)
+            f.write(TRAIT_CODE_BEGINNING + string + TRAIT_CODE_ENDING)
 
-#BUTTONS:
+
+# ---------------------------------------------------------- #
+
+
+
+# ----------------------BUTTONS ---------------------- #
+
 traitCreate = ctk.CTkButton(root, text="Create Trait", width=100, fg_color="#fcf9ff", text_color="#101519", corner_radius=5, command=traitCreate)
 write = ctk.CTkButton(root, text="Write", width=200, fg_color="#fcf9ff", text_color="#101519", corner_radius=5, command=write)
-#LABELS:
-idLabel = ctk.CTkLabel(root, text="Trait name: ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#fcf9ff")
-healthLabel = ctk.CTkLabel(root, text="Health: ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#fcf9ff")
-damageLabel = ctk.CTkLabel(root, text="Damage: ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#fcf9ff")
-attackSpeedLabel = ctk.CTkLabel(root, text="Attack Speed: ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#fcf9ff")
-critChanceLabel = ctk.CTkLabel(root, text="Crit Chance: ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#fcf9ff")
-rangeLabel = ctk.CTkLabel(root, text="Range: ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#fcf9ff")
-accLabel = ctk.CTkLabel(root, text="Accuracy: ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#fcf9ff")
-speedLabel = ctk.CTkLabel(root, text="Speed: ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#fcf9ff")
-dodgeLabel = ctk.CTkLabel(root, text="Dodge Chance: ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#fcf9ff")
-intelligenceLabel = ctk.CTkLabel(root, text="Intelligence: ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#fcf9ff")
-scaleLabel = ctk.CTkLabel(root, text="Scale: ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#fcf9ff")
-descLabel = ctk.CTkLabel(root, text="Description: ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#fcf9ff")
-effectsLabel = ctk.CTkLabel(root, text="Attack Effects: ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#fcf9ff")
-#ENTRY POINTS:
-inputTraitId = ctk.CTkEntry(root, border_color="#1D3142", fg_color="#203547", text_color="#D0D0E1")
+
+# ---------------------------------------------------------- #
+
+
+
+# ---------------------- LABLES ---------------------- #
+def new_label(string):
+     return ctk.CTkLabel(root, text=string, font=ctk.CTkFont(size=15, weight="bold"), text_color="#fcf9ff")
+     
+def create_labels_traits(): 
+    idLabel = new_label("Trait Name: ")
+    healthLabel = new_label("Health: ")
+    damageLabel = new_label("Damage: ")
+    attackSpeedLabel = new_label("Attack Speed: ")
+    critChanceLabel = new_label("Crit Chance: ")
+    rangeLabel = new_label("Range: ")
+    accLabel = new_label("Accuracy: ")
+    speedLabel = new_label("Speed: ")
+    dodgeLabel = new_label("Dodge Chance: ")
+    intelligenceLabel = new_label("Intelligence: ")
+    scaleLabel = new_label("Scale: ")
+    descLabel = new_label("Description: ")
+    effectsLabel = new_label("Attack Effects: ")
+
+    return [
+    idLabel, healthLabel, damageLabel, attackSpeedLabel, critChanceLabel, 
+    rangeLabel, accLabel, speedLabel, dodgeLabel, intelligenceLabel, 
+    scaleLabel, descLabel, effectsLabel
+    ]
+
+def create_labels_effects(): 
+    idLabel = new_label("Effect Name: ")
+    duration = new_label("Duration: ")
+    healthLabel = new_label("Health: ")
+    damageLabel = new_label("Damage: ")
+    attackSpeedLabel = new_label("Attack Speed: ")
+    critChanceLabel = new_label("Crit Chance: ")
+    rangeLabel = new_label("Range: ")
+    accLabel = new_label("Accuracy: ")
+    speedLabel = new_label("Speed: ")
+    dodgeLabel = new_label("Dodge Chance: ")
+    intelligenceLabel = new_label("Intelligence: ")
+    knockbackLabel = new_label("KnockBack")
+    knockbackRLabel = new_label("KnockBack Reduction")
+    descLabel = new_label("Description: ")
+
+    return [
+    idLabel, healthLabel, damageLabel, attackSpeedLabel, critChanceLabel, 
+    rangeLabel, accLabel, speedLabel, dodgeLabel, intelligenceLabel, 
+    descLabel, knockbackLabel, knockbackRLabel
+    ]
+
+# ---------------------------------------------------------- #
+
+
+
+# ---------------------- ENTRY POINTS ---------------------- #
+
+def new_entry():
+    return ctk.CTkEntry(root, border_color="#1D3142", fg_color="#203547", text_color="#D0D0E1")
+
+inputTraitId = new_entry()
 inputTraitId.insert(0, "randoTrait")
-health = ctk.CTkEntry(root, border_color="#1D3142", fg_color="#203547", text_color="#D0D0E1")
+health = new_entry()
 health.insert(0, 0)
-damage = ctk.CTkEntry(root, border_color="#1D3142", fg_color="#203547", text_color="#D0D0E1")
+damage = new_entry()
 damage.insert(0, 0)
-attackSpeed = ctk.CTkEntry(root, border_color="#1D3142", fg_color="#203547", text_color="#D0D0E1")
+attackSpeed = new_entry()
 attackSpeed.insert(0, 0)
-criticalChance = ctk.CTkEntry(root, border_color="#1D3142", fg_color="#203547", text_color="#D0D0E1")
+criticalChance = new_entry()
 criticalChance.insert(0, 0.0)
-rangeT = ctk.CTkEntry(root, border_color="#1D3142", fg_color="#203547", text_color="#D0D0E1")
+rangeT = new_entry()
 rangeT.insert(0, 0)
-accuracy = ctk.CTkEntry(root, border_color="#1D3142", fg_color="#203547", text_color="#D0D0E1")
+accuracy = new_entry()
 accuracy.insert(0, 0)
-speed = ctk.CTkEntry(root, border_color="#1D3142", fg_color="#203547", text_color="#D0D0E1")
+speed = new_entry()
 speed.insert(0, 0)
-dodge = ctk.CTkEntry(root, border_color="#1D3142", fg_color="#203547", text_color="#D0D0E1")
+dodge = new_entry()
 dodge.insert(0, 0.0)
-intelligence = ctk.CTkEntry(root, border_color="#1D3142", fg_color="#203547", text_color="#D0D0E1")
+intelligence = new_entry()
 intelligence.insert(0, 0)
-scale = ctk.CTkEntry(root, border_color="#1D3142", fg_color="#203547", text_color="#D0D0E1")
+scale = new_entry()
 scale.insert(0, 0.0)
-description = ctk.CTkEntry(root, border_color="#1D3142", fg_color="#203547", text_color="#D0D0E1")
+description = new_entry()
 description.insert(0, "This Is My First Mod!")
+
+# ---------------------------------------------------------- #
+
+
+
+# ---------------------- OPTIONS ---------------------- #
 
 OPTIONS = [
      "BurningEffect",
@@ -172,21 +234,17 @@ options = StringVar()
 options.set("BurningEffect")
 dropdown = OptionMenu(root, options, *OPTIONS)
 
-#FORMATTING:
-idLabel.grid(row=0, column=0, padx=2, pady=4)
-healthLabel.grid(row=1, column=0, padx=2, pady=4)
-damageLabel.grid(row=2, column=0, padx=2, pady=4)
-attackSpeedLabel.grid(row=3, column=0, padx=2, pady=4)
-critChanceLabel.grid(row=4, column=0, padx=2, pady=4)
-rangeLabel.grid(row=5, column=0, padx=2, pady=4)
-accLabel.grid(row=6, column=0, padx=2, pady=4)
-speedLabel.grid(row=7, column=0, padx=2, pady=4)
-dodgeLabel.grid(row=8, column=0, padx=2, pady=4)
-intelligenceLabel.grid(row=9, column=0, padx=2, pady=4)
-scaleLabel.grid(row=10, column=0, padx=2, pady=4)
-effectsLabel.grid(row=11, column=0, padx=2, pady=4)
-descLabel.grid(row=12, column=0, padx=2, pady=4)
+# ---------------------------------------------------------- #
 
+
+
+
+# ---------------------- FORMATTING ---------------------- #
+def setup_labels():
+    labels = create_labels_traits()
+    for i, label in enumerate(labels):
+        label.grid(row=i, column=0, padx=2, pady=4)
+        
 
 inputTraitId.grid(row=0, column=1, padx=2, pady=4)
 health.grid(row=1, column=1, padx=2, pady=4)
@@ -206,8 +264,8 @@ description.grid(row=12, column=1, padx=2, pady=4)
 traitCreate.grid(row=13, column=1, padx=1, pady=10)
 write.grid(row=14, column=1, padx=1, pady=10)
 
+setup_labels()
+# ---------------------------------------------------------- #
 
 
-        
-
-root.mainloop()
+root.mainloop() #Main Loop
